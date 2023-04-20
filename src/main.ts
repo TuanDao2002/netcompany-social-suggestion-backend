81e337688785b0442f0eac069b67141cc0b6f8e7
TuanDao2002 <tuan09092002@gmail.com>
2023-04-20 16:25:10 +0700

    fix cors identityMetadata

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:5173'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Authorization'],
-  });
-  await app.listen(8080);
+  app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+  await app.listen(3000);
 }
 bootstrap();
