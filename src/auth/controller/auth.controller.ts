b84dde5802b394009cfaab164688e4ff50569bee
TuanDao2002 <tuan09092002@gmail.com>
2023-04-20 15:02:18 +0700

    update SSO microsoft

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,27 +1,36 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Post, Req, UseGuards } from '@nestjs/common';
+import { Strategy } from 'passport-azure-ad';
+import { SigninDto } from '../dto/signin.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { AuthService } from '../service/auth.service';
-import { SigninDto } from "../dto/signin.dto";
-
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post()
-  signin(@Body() signinDto: SigninDto) {
-    return this.authService.signin(signinDto);
+  @UseGuards(AuthGuard(Strategy))
+  signin(@Req() request) {
+    const token = this.jwtService.sign({ sub: request.user.oid });
+    console.log("🚀 ~ file: auth.controller.ts:20 ~ AuthController ~ signin ~ token:", token)
+
+    return {
+      username: request.user.displayName,
+      email: request.user.mail,
+      accessToken: token,
+    };
   }
 
   @Delete()
   signout() {
     return this.authService.signout();
   }
+}
+function AuthGuard(Strategy: any): any {
+  throw new Error('Function not implemented.');
 }
