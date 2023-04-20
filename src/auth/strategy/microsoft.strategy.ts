60677644614fc98ae38af2706c3c9fe8d51db2a6
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-04-20 16:08:41 +0700

    fix backend cors

--- src/auth/strategy/microsoft.strategy.ts
+++ src/auth/strategy/microsoft.strategy.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { OIDCStrategy } from 'passport-azure-ad';
 import { ConfigService } from '@nestjs/config';
-import { AuthService } from "../service/auth.service";
+import { AuthService } from '../service/auth.service';
 
 @Injectable()
-export class MicrosoftStrategy extends PassportStrategy(OIDCStrategy, 'azure-ad') {
+export class MicrosoftStrategy extends PassportStrategy(
+  OIDCStrategy,
+  'azure-ad',
+) {
   constructor(
     private readonly authService: AuthService,
     private readonly configService: ConfigService,
@@ -14,11 +17,13 @@ export class MicrosoftStrategy extends PassportStrategy(OIDCStrategy, 'azure-ad'
       clientID: configService.get('AZURE_AD_CLIENT_ID'),
       clientSecret: configService.get('AZURE_AD_CLIENT_SECRET'),
       redirectUrl: `${configService.get('APP_BASE_URL')}`,
-      identityMetadata: `https://login.microsoftonline.com/${configService.get('AZURE_AD_TENANT_ID')}/.well-known/openid-configuration`,
+      identityMetadata: `https://login.microsoftonline.com/${configService.get(
+        'AZURE_AD_TENANT_ID',
+      )}/.well-known/openid-configuration`,
       responseType: 'code id_token',
       responseMode: 'form_post',
       scope: ['openid', 'profile', 'email'],
-      allowHttpForRedirectUrl: true
+      allowHttpForRedirectUrl: true,
     });
   }
 
