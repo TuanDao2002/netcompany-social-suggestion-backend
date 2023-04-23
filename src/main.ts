76ff9b3a2cfa56f7dbdcc881a8dd9d40a02c11c5
TuanDao2002 <tuan09092002@gmail.com>
2023-04-23 19:46:18 +0700

    fix/auth: fix cors by add OPTIONS in vercel.json

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,10 @@ async function bootstrap() {
     }),
   );
   const configService = app.get<ConfigService>(ConfigService);
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+    origin: configService.get('APP_BASE_URL'),
+  });
   await app.listen(CommonConstant.PORT);
   console.log(`Server listening on port: ${CommonConstant.PORT}`);
 }
