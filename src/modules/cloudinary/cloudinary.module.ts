cf4a1e51ac3bcfda35e42169d559dead176c661a
TuanDao2002 <tuan09092002@gmail.com>
2023-04-29 11:34:59 +0700

    feat/image: use stream to remove temp folder

--- src/modules/cloudinary/cloudinary.module.ts
+++ src/modules/cloudinary/cloudinary.module.ts
@@ -1,23 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CloudinaryService } from './service/cloudinary.service';
-import { CloudinaryController } from './controller/cloudinary.controller';
 import { ConfigModule } from '@nestjs/config';
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import { join } from 'path';
+import { CloudinaryController } from './controller/cloudinary.controller';
+import { CloudinaryService } from './service/cloudinary.service';
 
 @Module({
   imports: [
     ConfigModule,
-    MulterModule.register({
-      storage: diskStorage({
-        destination: '/tmp',
-        filename: (req, file, cb) => {
-          const filename: string = file.originalname.replace(/\s+/g, '-');
-          cb(null, `${filename}`);
-        },
-      }),
-    }),
   ],
   controllers: [CloudinaryController],
   providers: [CloudinaryService],
--- src/modules/cloudinary/service/cloudinary.service.ts
+++ src/modules/cloudinary/service/cloudinary.service.ts
@@ -1,8 +1,8 @@
 import { v2 as cloudinary } from 'cloudinary';
-import fs from 'fs';
 
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Readable } from 'stream';
 @Injectable()
 export class CloudinaryService {
   constructor(private configService: ConfigService) {
@@ -12,22 +12,31 @@ export class CloudinaryService {
       api_secret: this.configService.get('CLOUDINARY_API_SECRET'),
     });
   }
-  async uploadImage(file: Express.Multer.File): Promise<{ image: string }> {
+  async uploadImage(file: Express.Multer.File) {
     if (!file) {
       throw new BadRequestException('No File Uploaded');
     }
     if (!file.mimetype.startsWith('image')) {
-      fs.unlinkSync(file.path);
       throw new BadRequestException('Please Upload Image');
     }
 
-    const result = await cloudinary.uploader.upload(file.path, {
-      use_filename: true,
-      folder: 'file-upload',
+    const stream = new Readable();
+    stream.push(file.buffer);
+    stream.push(null);
+
+    return new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        {folder: 'image-upload'},
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve({ image: result.secure_url });
+          }
+        },
+      );
+
+      stream.pipe(uploadStream);
     });
-
-    fs.unlinkSync(file.path);
-
-    return { image: result.secure_url };
   }
 }
