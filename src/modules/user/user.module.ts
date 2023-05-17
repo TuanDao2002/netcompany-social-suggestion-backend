ff04d34f4e217a429df40af73715fe5a08c90d05
TuanDao2002 <tuan09092002@gmail.com>
2023-05-17 21:15:42 +0700

    fix/user: add Auth module to use JwtGuard

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,9 +4,11 @@ import { Module } from '@nestjs/common';
 import { UserRepository } from './repository/user.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/users.schema';
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
   imports: [
+    AuthModule,
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UserController],
